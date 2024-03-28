5e3cefeb88f9ec940768f010535c391bfd632e9e
Damian Sieradzki <damian.sieradzki@hotmail.com>
2024-03-28 10:23:23 +0100

    feat: Remove neorg

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -119,39 +119,6 @@ return {
     end,
   },
   {
-    'nvim-neorg/neorg',
-    build = ':Neorg sync-parsers',
-    lazy = false, -- specify lazy = false because some lazy.nvim distributions set lazy = true by default
-    tag = "v7.0.0",
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      require('neorg').setup {
-        load = {
-          ['core.defaults'] = {}, -- Loads default behaviour
-          ['core.concealer'] = {
-            config = {
-              icon_preset = 'varied',
-              icons = {
-                todo = {
-                  undone = {
-                    icon = ' ',
-                  },
-                },
-              },
-            },
-          }, -- Adds pretty icons to your documents
-          -- ['core.dirman'] = { -- Manages Neorg workspaces
-          -- config = {
-          --   workspaces = {
-          --     notes = '~/notes',
-          --   },
-          -- },
-          --},
-        },
-      }
-    end,
-  },
-  {
     'folke/noice.nvim',
     event = 'VeryLazy',
     opts = {
